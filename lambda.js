9396f4dfa2e9fbd28e78602965214c76a4512e25
Ashik Nesin <mail@ashiknesin.com>
2021-04-05 11:45:48 +0530

    set .callbackWaitsForEmptyEventLoop as false

--- lambda.js
+++ lambda.js
@@ -11,4 +11,16 @@ require('source-map-support/register')
 
 import app from './app';
 import serverless from 'serverless-http';
-exports.handler = serverless(app);
+// exports.handler = serverless(app);
+
+// or as a promise
+const handler = serverless(app);
+exports.handler = async (event, context) => {
+    // Make sure to add this so you can re-use `conn` between function calls.
+    // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    const result = await handler(event, context);
+    // and here
+    return result;
+};
