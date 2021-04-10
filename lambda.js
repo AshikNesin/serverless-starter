45b085d98c3e795264511f576d74e0dada2c24c1
Ashik Nesin <mail@ashiknesin.com>
2021-04-10 20:35:47 +0530

    chore: commit save point

--- lambda.js
+++ lambda.js
@@ -1,15 +1,5 @@
 require('source-map-support/register')
 import { connectToDatabase } from './utils/db'
-
-// exports.handler = function (event, context, callback) {
-//     console.log(`Hello, Cloudwatch!`);
-//     const response = {
-//         statusCode: 200,
-//         body: 'Hello Lambda!',
-//     };
-//     return callback(null, response);
-// };
-
 import app from './app';
 import serverless from 'serverless-http';
 // or as a promise
